ee3d259a0f5651d507f2b6e8fdd313e22f6cf8f3
noituri <yknomeh@protonmail.com>
2020-08-28 17:11:29 +0200

    4coder

--- components.c
+++ components.c
@@ -11,14 +11,14 @@ void registerComponents(Ecs *world)
     ecs_register_component(world, COMPONENT_TRANSFORM, 1000, sizeof(CTransform), NULL);
     ecs_register_component(world, COMPONENT_PLAYER_STATE, 1000, sizeof(CPlayerState), NULL);
     ecs_register_component(world, COMPONENT_SPRITE, 1000, sizeof(CSprite), NULL);
-//    ecs_register_component(world, COMPONENT_PHYSICS, 1000, sizeof(CPhysics), NULL);
+    //    ecs_register_component(world, COMPONENT_PHYSICS, 1000, sizeof(CPhysics), NULL);
 }
 
 void createTerrain(Ecs *world, int width, int height, int seed)
 {
     EcsEnt entity = ecs_ent_make(world);
     Vector2* blocks = malloc(width * height * sizeof(Vector2));
-
+    
     int i = 0;
     for (int x = 0; x < (width / 16); x++) {
         int maxY = (int)(Perlin_Get2d((double)x / 10, 0, 0.5, 4, seed) * 100);
@@ -27,18 +27,18 @@ void createTerrain(Ecs *world, int width, int height, int seed)
             i++;
         }
     }
-
+    
     printf("Generated %d blocks\n", i);
-
+    
     CTerrain terrain = {
-            .blocks = blocks,
-            .blocksSize = i,
-            .width = width,
-            .height = height,
-            .seed = seed
+        .blocks = blocks,
+        .blocksSize = i,
+        .width = width,
+        .height = height,
+        .seed = seed
     };
     CTransform transform = { 720.0f, 100.0f };
-
+    
     ecs_ent_add_component(world, entity,COMPONENT_TERRAIN, &terrain);
     ecs_ent_add_component(world, entity, COMPONENT_TRANSFORM, &transform);
 }
@@ -48,38 +48,38 @@ void createPlayer(Ecs *world)
     EcsEnt entity = ecs_ent_make(world);
     CPlayerState pState = PLAYER_IDLE;
     CTransform transform = { 720.0f, -150.0f };
-
+    
     CSprite sprite = {
-            .sprite = playerTxt,
-            .sprites = 9,
-            .width = playerTxt.width / 9,
-            .height = 64 * 2,
-            .frame = 0
+        .sprite = playerTxt,
+        .sprites = 9,
+        .width = playerTxt.width / 9,
+        .height = 64 * 2,
+        .frame = 0
     };
-
-//    CPhysics body = CreatePhysicsBodyRectangle(transform, (float) playerTxt.width / 9, 64 * 2, 1);
-//    body->freezeOrient = true;
-
+    
+    //    CPhysics body = CreatePhysicsBodyRectangle(transform, (float) playerTxt.width / 9, 64 * 2, 1);
+    //    body->freezeOrient = true;
+    
     ecs_ent_add_component(world, entity, COMPONENT_PLAYER_STATE, &pState);
     ecs_ent_add_component(world, entity, COMPONENT_TRANSFORM, &transform);
     ecs_ent_add_component(world, entity, COMPONENT_SPRITE, &sprite);
-//    ecs_ent_add_component(world, entity, COMPONENT_PHYSICS, &body);
+    //    ecs_ent_add_component(world, entity, COMPONENT_PHYSICS, &body);
 }
 
 void renderCollisions()
 {
-//	int bodiesCount = GetPhysicsBodiesCount();
-//	for (int i = 0; i < bodiesCount; i++) {
-//		PhysicsBody body = GetPhysicsBody(i);
-//
-//		int vertexCount = GetPhysicsShapeVerticesCount(i);
-//		for (int j = 0; j < vertexCount; j++) {
-//			Vector2 vertexA = GetPhysicsShapeVertex(body, j);
-//
-//			int jj = (((j + 1) < vertexCount) ? (j + 1) : 0);
-//			Vector2 vertexB = GetPhysicsShapeVertex(body, jj);
-//
-//			DrawLineV(vertexA, vertexB, RED);
-//		}
-//	}
+    //	int bodiesCount = GetPhysicsBodiesCount();
+    //	for (int i = 0; i < bodiesCount; i++) {
+    //		PhysicsBody body = GetPhysicsBody(i);
+    //
+    //		int vertexCount = GetPhysicsShapeVerticesCount(i);
+    //		for (int j = 0; j < vertexCount; j++) {
+    //			Vector2 vertexA = GetPhysicsShapeVertex(body, j);
+    //
+    //			int jj = (((j + 1) < vertexCount) ? (j + 1) : 0);
+    //			Vector2 vertexB = GetPhysicsShapeVertex(body, jj);
+    //
+    //			DrawLineV(vertexA, vertexB, RED);
+    //		}
+    //	}
 }
--- systems.c
+++ systems.c
@@ -24,17 +24,17 @@ void renderTerrain(Ecs *world)
         if (ecs_ent_has_mask(world, entity, RENDER_TERRAIN_SYSTEM_MASK)) {
             CTerrain *terrain = ecs_ent_get_component(world, entity, COMPONENT_TERRAIN);
             Vector2 pos = camera.target;
-
+            
             float rightOffset = (pos.x + (float) SCREEN_WIDTH / 2.0f) + 40.0f;
             float leftOffset = (pos.x - (float) SCREEN_WIDTH / 2.0f) - 40.0f;
             float bottomOffset = (pos.y + (float) SCREEN_HEIGHT / 2.0f) + 40.0f;
             float topOffset = (pos.y - (float) SCREEN_HEIGHT / 2.0f) - 40.0f;
-
+            
             for (int j = 0; j < terrain->blocksSize; j++) {
                 Vector2 tmpBlock = terrain->blocks[j];
                 if (tmpBlock.x > rightOffset || tmpBlock.x < leftOffset || tmpBlock.y > bottomOffset || tmpBlock.y < topOffset)
                     continue;
-
+                
                 DrawTextureRec(dirtTxt, (Rectangle) {40, 100, 16, 16}, tmpBlock, WHITE);
             }
 		}
@@ -48,30 +48,30 @@ void movePlayer(Ecs *world)
     for (uint32_t i = 0; i < ecs_for_count(world); i++) {
         EcsEnt entity = ecs_get_ent(world, i);
         if (ecs_ent_has_mask(world, entity, MOVE_PLAYER_SYSTEM_MASK)) {
-//            CPhysics *body = ecs_ent_get_component(world, entity, COMPONENT_PHYSICS);
-//            CTransform *pos = ecs_ent_get_component(world, entity, COMPONENT_TRANSFORM);
-//            Vector2 newPos = (*body)->position;
-
-//            if (pos->x != newPos.x || pos->y != newPos.y)
-//            	posChanged = true;
-//            *pos = (*body)->position;
-//            if (IsKeyDown(KEY_A)) {
-//                camera.target.x -= 10;
-//            }
-//
-//            if (IsKeyDown(KEY_D)) {
-//                camera.target.x += 10;
-//            }
-//
-//            if (IsKeyDown(KEY_W)) {
-//                camera.target.y -= 10;
-//            }
-//
-//            if (IsKeyDown(KEY_S)) {
-//                camera.target.y += 10;
-//            }
-
-//            *pos = camera.target;
+            //            CPhysics *body = ecs_ent_get_component(world, entity, COMPONENT_PHYSICS);
+            //            CTransform *pos = ecs_ent_get_component(world, entity, COMPONENT_TRANSFORM);
+            //            Vector2 newPos = (*body)->position;
+            
+            //            if (pos->x != newPos.x || pos->y != newPos.y)
+            //            	posChanged = true;
+            //            *pos = (*body)->position;
+            //            if (IsKeyDown(KEY_A)) {
+            //                camera.target.x -= 10;
+            //            }
+            //
+            //            if (IsKeyDown(KEY_D)) {
+            //                camera.target.x += 10;
+            //            }
+            //
+            //            if (IsKeyDown(KEY_W)) {
+            //                camera.target.y -= 10;
+            //            }
+            //
+            //            if (IsKeyDown(KEY_S)) {
+            //                camera.target.y += 10;
+            //            }
+            
+            //            *pos = camera.target;
         }
     }
 }
@@ -85,20 +85,20 @@ void renderSprite(Ecs *world)
         if (ecs_ent_has_mask(world, entity, RENDER_SPRITE_SYSTEM_MASK)) {
             CTransform *pos = ecs_ent_get_component(world, entity, COMPONENT_TRANSFORM);
             CSprite *sprite = ecs_ent_get_component(world, entity, COMPONENT_SPRITE);
-
+            
             Rectangle rec = {
-                    .width = (float) sprite->width,
-                    .height = (float) sprite->height,
-                    .x = (float) sprite->frame * (float) sprite->width,
-                    .y = 0
+                .width = (float) sprite->width,
+                .height = (float) sprite->height,
+                .x = (float) sprite->frame * (float) sprite->width,
+                .y = 0
             };
             frameCounter++;
             if (frameCounter % 10 == 0)
                 sprite->frame++;
-
+            
             if (frameCounter == 60)
                 frameCounter = 0;
-
+            
             if (sprite-> frame > sprite->sprites)
                 sprite->frame = 0;
             DrawTextureRec(sprite->sprite, rec, *pos, WHITE);
