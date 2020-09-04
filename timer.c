43cfd5330fb43417dbbb39910d7df0cca0017d7f
Mikolaj Radkowski <macbooc48@gmail.com>
2020-09-04 19:22:15 +0200

    Fixes for windows

--- timer.c
+++ timer.c
@@ -1,25 +1,30 @@
+#ifdef WIN32
+#include <Windows.h>
+typedef LARGE_INTEGER Clock;
+LARGE_INTEGER cpu_freq;
+#else
+#include <time.h>
+typedef struct timespec Clock;
+#endif
+
 #include "timer.h"
 
 Clock start;
 
+void GetClockTime(Clock *t)
+{
 #ifdef WIN32
-LARGE_INTEGER cpu_freq;
+	QueryPerformanceCounter(t);
+#else
+	clock_gettime(CLOCK_MONOTONIC, t);
 #endif
+}
 
 void InitTimer()
 {
     GetClockTime(&start);
 #ifdef WIN32
     QueryPerformanceFrequency(&cpu_freq);
-#endif
-}
-
-void GetClockTime(Clock *t)
-{
-#ifdef WIN32
-    QueryPerformanceCounter(t);
-#else
-    clock_gettime(CLOCK_MONOTONIC, t);
 #endif
 }
 
--- timer.h
+++ timer.h
@@ -1,17 +1,7 @@
 #ifndef ANDORA_TIMER_H
 #define ANDORA_TIMER_H
 
-#ifdef WIN32
-// Not tested
-#include <Windows.h>
-typedef LARGE_INTEGER Clock;
-#else
-#include <time.h>
-typedef struct timespec Clock;
-#endif
-
 void InitTimer();
-void GetClockTime(Clock *t);
 float ClockTimeElapsed();
 
 #endif //ANDORA_TIMER_H
